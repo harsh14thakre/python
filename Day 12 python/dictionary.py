a0b76fa8be2303dec4c214a6a91ef3de5cd0f7e3
harsh14thakre <harsh14thakre@gmail.com>
2025-03-28 18:29:23 +0530

    updated

---
+++ Day 12 python/dictionary.py
@@ -0,0 +1,11 @@
+# Collection of elements (Pair of key value)
+# represented in {} with comma seperated elements
+# Key must be unque
+# Value may be duplicate
+# Indexing not supported
+# Slicing not supported
+# Mutable in nature
+# E.g=> d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
+
+d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
+print(d)
