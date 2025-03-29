1027551d8cb0b041abcdfafd6c51af6bffcc04cd
harsh14thakre <harsh14thakre@gmail.com>
2025-03-29 18:25:41 +0530

    commit

--- Day 12 python/dictionary.py
+++ Day 12 python/dictionary.py
@@ -7,7 +7,7 @@
 # Mutable in nature
 # E.g=> d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
 
-# # d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
+d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
 # # print(d)
 # # print(max(d))
 # # print(min(d))
@@ -30,5 +30,22 @@
 # # print(d1)
 
 # l='Harsh'
-# d1=dict.fromkeys(l)
+# d1=dict.fromkeys(l,100)
 # print(d1)
+
+# print(d.get('age'))
+# print(d.items())
+# print(d.keys())
+# print(d.values())
+# print(d.popitem())
+# print(d)
+# d.pop('age')
+# print(d)
+
+
+# d.setdefault('name','rahul')
+# print(d)
+
+
+# d.setdefault('greed','M.tech')
+# print(d)
