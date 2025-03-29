71a8f610bbe24af34c91ec172851422557deb6c8
harsh14thakre <harsh14thakre@gmail.com>
2025-03-29 18:06:37 +0530

    commit

--- Day 12 python/dictionary.py
+++ Day 12 python/dictionary.py
@@ -7,5 +7,28 @@
 # Mutable in nature
 # E.g=> d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
 
-d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
-print(d)
+# # d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
+# # print(d)
+# # print(max(d))
+# # print(min(d))
+# # print(type(d))
+# # print(id(d))
+
+
+
+# # Methods of dictionary
+
+# # print(d.clear())
+# # print(d)
+
+# # x=d.copy()
+# # print(x,d)
+# # print(id(x),id(d))
+
+# # l=['name','age','qualification']
+# # d1=dict.fromkeys(l)
+# # print(d1)
+
+# l='Harsh'
+# d1=dict.fromkeys(l)
+# print(d1)
---
+++ Day 12 python/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+d={'name':'Harsh', 'age':37,'Qualification':'M.tech'}
