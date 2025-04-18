b0757ea23ea3279e09ec245918f6e9b707c3b8ed
harsh14thakre <harsh14thakre@gmail.com>
2025-04-18 18:00:32 +0530

    updated

---
+++ functions/paramterized.py
@@ -0,0 +1,20 @@
+# #############################Relation between parameters#####################################################
+
+# def Add(x,y):
+#     print(x,y)
+#     return x+y
+# a=10
+# b=4
+# z=Add(x=a,y=b)
+# print(z)
+
+
+def Add(x,y):
+    print(x,y)
+    return x+y
+a=int(input("Enter any number"))
+b=int(input("Enter any number"))
+z=Add(x=a,y=b)
+print(z)
+
+
