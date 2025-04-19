1dc62570bbdb277d1b24499cf0d4e9d06dea63e9
harsh14thakre <harsh14thakre@gmail.com>
2025-04-19 18:16:40 +0530

    updated

--- functions/paramterized.py
+++ functions/paramterized.py
@@ -9,12 +9,88 @@
 # print(z)
 
 
-def Add(x,y):
-    print(x,y)
-    return x+y
-a=int(input("Enter any number"))
-b=int(input("Enter any number"))
-z=Add(x=a,y=b)
-print(z)
+# def Add(x,y):
+#     print(x,y)
+#     return x+y
+# a=int(input("Enter any number"))
+# b=int(input("Enter any number"))
+# z=Add(x=a,y=b)
+# print(z)
+
+# def add(*n):
+#     print(n)
+#     print(type(n))
+
+# add()
+# add(10)    
 
 
+# variable length argument
+
+# def add(*n):
+#     sum=0
+#     for i in n:
+#         sum=sum+i
+#     return sum
+# z=add(2,3,4,5,6,4,7)    
+# print(z)
+
+
+# def add(*n):
+#     sum=0
+#     for i in n:
+#         for j in i:
+#             sum=sum+j
+#     return sum
+# n=eval(input("Enter all values"))
+# z=add(n)        
+# print(z)
+
+
+# #######################################keyword variable length argumments########################################
+
+# def show(**n):
+#     print(n)
+#     print(type(n))
+#     for i,j in n.items():
+#         print(i,j)
+
+# show(name="Harsh",age=20)        
+
+
+
+
+
+
+# relation between parameter and arguments
+
+# x=10
+# if x:
+#     x=20
+#     print(x)
+# print(x)    
+
+# ----------------------------------------------------------------------------------
+
+# x=10
+# def display():
+#     x=20
+#     print(x)
+# display()    
+# print(x) 
+# 
+
+
+x=10
+def display():
+    x=20
+    print(x)
+    print(id(x))
+display()    
+print(x)       
+
+# x=10
+# if x:
+#     x=20
+#     print(x)
+#     print(id(x))    
---
+++ functions/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+x=10
+# if x:
+#     x=20
+#     print(x)
+#     print(id(x))    
